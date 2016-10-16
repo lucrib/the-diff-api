1fd6439784c0e3422d9e4ae152a233e091a3cdad
Lucas Ribeiro <lucasribeiro1990@gmail.com>
2016-10-16 14:26:26 -0200

    Remove unused imports

--- app/rest_api.py
+++ app/rest_api.py
@@ -1,7 +1,6 @@
 import base64
-import json
-from flask import jsonify, abort, make_response, url_for
-from flask_restful import Resource, reqparse, fields, marshal
+from flask import jsonify, abort, make_response
+from flask_restful import Resource, reqparse
 from flask_httpauth import HTTPBasicAuth
 from app import db
 from app.models import DiffModel
